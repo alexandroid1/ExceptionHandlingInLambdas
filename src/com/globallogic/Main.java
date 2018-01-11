04f3ed2149c520981790e9b79d63319062ac9933
alexandroid1 <alex123456>
2018-01-11 19:12:49 +0200

    wrapperLambda

--- src/com/globallogic/Main.java
+++ src/com/globallogic/Main.java
@@ -8,19 +8,23 @@ public class Main {
         int[] someNumbers = {1, 2, 3, 4};
         int key = 0;
 
-        process(someNumbers, key, (v,k) -> {
-            try {
-                System.out.println(v / k);
-            }
-            catch (ArithmeticException e) {
-                System.out.println("An Arithmetic Exception happened");
-            }
-        });
+        process(someNumbers, key, wrapperLambda((v,k) -> System.out.println(v / k)));
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
         for (int i:someNumbers) {
             consumer.accept(i, key);
         }
+    }
+
+    private static BiConsumer<Integer, Integer> wrapperLambda(BiConsumer<Integer, Integer> consumer){
+        return (v,k) -> {
+            try {
+                consumer.accept(v,k);
+            }
+            catch (ArithmeticException e){
+                System.out.println("Exeption caught in wrapper lambda");
+            }
+        };
     }
 }
