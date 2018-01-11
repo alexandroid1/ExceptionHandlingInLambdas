eb92235b60bec426681501609907216007afc72c
alexandroid1 <alex123456>
2018-01-11 17:52:35 +0200

    try..catch in lambda

--- src/com/globallogic/Main.java
+++ src/com/globallogic/Main.java
@@ -6,9 +6,16 @@ public class Main {
 
     public static void main(String[] args) {
         int[] someNumbers = {1, 2, 3, 4};
-        int key = 2;
+        int key = 0;
 
-        process(someNumbers, key, (v,k) -> System.out.println(v+k));
+        process(someNumbers, key, (v,k) -> {
+            try {
+                System.out.println(v / k);
+            }
+            catch (ArithmeticException e) {
+                System.out.println("An Arithmetic Exception happened");
+            }
+        });
     }
 
     private static void process(int[] someNumbers, int key, BiConsumer<Integer, Integer> consumer) {
